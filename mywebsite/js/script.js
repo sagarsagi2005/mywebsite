77c2ed72ed08a1e5f31e176a69a95aa711ae6a66
sagarsagi2005 <sagarsriram535@gmail.com>
2025-11-30 22:39:49 +0530

    Implement welcome alert on page load
    Add welcome alert on page load

--- mywebsite/js/script.js
+++ mywebsite/js/script.js
@@ -1,3 +1,8 @@
 document.getElementById("changeColorBtn").onclick = function(){
     document.body.style.backgroundColor = "#dfffe2";
 };
+// This will run when the page loads
+window.onload = function() {
+    alert("Welcome to my website!");
+};
+
